c9016b81730b87d518bbf367c24368c3d29f5c45
brad-bartlett <bbartlett222@gmail.com>
2020-01-22 14:27:09 -0500

    merging

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,9 +1,9 @@
 class AdventuresController < ApplicationController
 
-    def index
-        @adventures = Adventure.all
-        render json: @adventures, status: 200
-    end
+    # def index
+    #     @adventures = Adventure.all
+    #     render json: @adventures, status: 200
+    # end
 
     def create
         @adventure = Adventure.create(date: params["date"], snippet: params["snippet"], rating: params["rating"], park_id: params["park_id"])
@@ -26,11 +26,5 @@ class AdventuresController < ApplicationController
         @adventure.destroy!
         render json: @adventures, status: 200
     end
-
-# private
-
-#     def adventure_params   
-#         params.require(:adventure).permit(:snippet, :rating, :hiker_id, :park_id)   
-#     end   
-
+    
 end
